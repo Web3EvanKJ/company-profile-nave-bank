e6657a64991d1012aed0777f4ec1d3348906af44
Nave765 <web3evankeane@gmail.com>
2025-08-27 21:05:22 +0700

    Fix: Optimize SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,33 @@ import AccountType from "@/components/AccountType";
 import ServiceSection from "@/components/ServiceSection";
 import FeaturesSection from "@/components/FeaturesSection";
 import HeroSection from "@/components/HeroSection";
+import Image from "next/image";
 
 function App() {
   return (
     <>
-      <section
-        className="relative lg:h-[90vh] h-[70vh] flex items-center justify-start bg-cover bg-center text-white"
-        style={{
-          backgroundImage: "url('/assets/nave-sky-banner.png')",
-        }}
-      >
-        <div className="absolute inset-0 bg-white/50 " />
+      <section className="relative lg:h-[90vh] h-[70vh] flex items-center justify-start text-white overflow-hidden">
+        {/* Optimized background image */}
+        <Image
+          src="/assets/nave-sky-banner.png"
+          alt="Sky Banner"
+          fill
+          priority
+          className="object-cover object-center"
+          quality={75}
+        />
+
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-white/50" />
+
+        {/* Content */}
         <div className="relative z-10 text-left px-10">
-          {/* Hero Section */}
-          <section className=" w-full py-20">
+          <section className="w-full py-20">
             <HeroSection />
           </section>
         </div>
       </section>
+
       <main className="flex-1 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-5">
         {/* Features Section */}
         <section className="py-20">
